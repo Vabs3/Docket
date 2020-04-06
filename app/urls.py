a2b00d164c6984b2ad5f0c9dd504036eda73adf7
Vaibhav Grover <63092690+Vabs3@users.noreply.github.com>
2020-04-07 04:42:37 +0530

    slot booking feature added (#4)

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from app.views import CreateEvent, AddEventAvailableSlot, UserEventDetail, UserEventList, EventSlotDays, \
-    EventSlotHours, UserCreate, LoginView
+    EventSlotHours, UserCreate, LoginView, BookEvent, UserBookingList
 
 urlpatterns = [
     path("users/", UserCreate.as_view(), name="user_create"),
@@ -11,5 +11,7 @@ urlpatterns = [
     path("events/<int:pk>", UserEventDetail.as_view(), name="get_event_detail"),
     path("events/", UserEventList.as_view(), name="get_event_list"),
     path("slots/<int:event_id>/<str:date>/", EventSlotHours.as_view(), name="get_hour_slots"),
-    path("slots/<int:event_id>/", EventSlotDays.as_view(), name="get_day_slots")
+    path("slots/<int:event_id>/", EventSlotDays.as_view(), name="get_day_slots"),
+    path("book-event/", BookEvent.as_view(), name="book_event"),
+    path("bookings/", UserBookingList.as_view(), name="user_bookings"),
 ]
