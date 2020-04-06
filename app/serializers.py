6bd096c49dd06a7c8ccb86bbcc42d8820474aaf5
Vabs3 <vbs273+g@gmail.com>
2020-04-07 01:12:25 +0530

    User Authorization added Unit tests for Events and slots added

--- app/serializers.py
+++ app/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
-
+from rest_framework.authtoken.models import Token
 from .models import UserEvent, EventSlot, UserBooking
+from django.contrib.auth.models import User
 
 
 class UserEventSerializer(serializers.ModelSerializer):
@@ -21,3 +22,22 @@ class UserBookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserBooking
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        Token.objects.create(user=user)
+        return user
+
--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,70 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase, APIClient
+
 
 # Create your tests here.
+
+
+class TestUserEvent(APITestCase):
+    def setUp(self):
+        TestUserEvent.setup_user()
+        self.client = APIClient()
+        self.event_list_uri = '/events/'
+        self.create_event_uri = '/create-event/'
+
+    @staticmethod
+    def setup_user():
+        User = get_user_model()
+        return User.objects.create_user(
+            'test',
+            email='testuser@test.com',
+            password='test'
+        )
+
+    def test_create_event(self):
+        self.client.login(username="test", password="test")
+        params = {
+            "name": "interview",
+            "created_by": "1"
+        }
+        response = self.client.post(self.create_event_uri, params)
+        self.assertEqual(response.status_code, 201,
+                         'Expected Response Code 201, received {0} instead.'
+                         .format(response.status_code))
+
+    def test_event_list(self):
+        self.client.login(username="test", password="test")
+        response = self.client.get(self.event_list_uri)
+        self.assertEqual(response.status_code, 200,
+                         'Expected Response Code 200, received {0} instead.'
+                         .format(response.status_code))
+
+
+class TestEventSlots(APITestCase):
+    def setUp(self):
+        TestUserEvent.setup_user()
+        self.client = APIClient()
+        self.add_slot_uri = '/add-slot/'
+
+    @staticmethod
+    def setup_user():
+        User = get_user_model()
+        return User.objects.create_user(
+            'test',
+            email='testuser@test.com',
+            password='test'
+        )
+
+    def test_create_event(self):
+        self.client.login(username="test", password="test")
+        params = {
+                "event_id": "1",
+                "date": "2020-06-01",
+                "start_time": "2020-06-01T12:00",
+                "end_time": "2020-06-01T15:00"
+            }
+        response = self.client.post(self.add_slot_uri, params)
+        self.assertEqual(response.status_code, 201,
+                         'Expected Response Code 201, received {0} instead.'
+                         .format(response.status_code))
+
